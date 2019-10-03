9a5382b7cdaaf0ac3f5a18c85ad3aecbc5784b89
Tomasz Pęczek <tpeczek@gmail.com>
2019-10-03 22:55:08 +0200

    Upgrade to ASP.NET Core 3.0.0.

---
+++ Demo.AspNetCore.Changefeed/Middlewares/EndpointRouteBuilderExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+
+namespace Demo.AspNetCore.Changefeed.Middlewares
+{
+    internal static class EndpointRouteBuilderExtensions
+    {
+        public static IEndpointConventionBuilder MapWebSocketConnections(this IEndpointRouteBuilder endpoints, string pattern)
+        {
+            if (endpoints == null)
+            {
+                throw new ArgumentNullException(nameof(endpoints));
+            }
+
+            RequestDelegate pipeline = endpoints.CreateApplicationBuilder()
+               .UseMiddleware<WebSocketConnectionsMiddleware>()
+               .Build();
+
+            return endpoints.Map(pattern, pipeline);
+        }
+    }
+}
--- Demo.AspNetCore.Changefeed/Startup.cs
+++ Demo.AspNetCore.Changefeed/Startup.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Lib.AspNetCore.ServerSentEvents;
 using Demo.AspNetCore.Changefeed.Middlewares;
@@ -45,11 +45,9 @@ namespace Demo.AspNetCore.Changefeed
             services.AddWebSocketConnections();
 
             services.AddThreadStats();
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider services)
+        public void Configure(IApplicationBuilder app, IHostEnvironment env, IServiceProvider services)
         {
             if (env.IsDevelopment())
             {
@@ -58,18 +56,18 @@ namespace Demo.AspNetCore.Changefeed
 
             services.GetService<IThreadStatsChangefeedDbService>().EnsureDatabaseCreated();
 
-            app.MapServerSentEvents("/thread-stats-changefeed");
-
-            app.UseWebSockets().MapWebSocketConnections("/thread-stats-changefeed-fallback");
-
-            app.UseStaticFiles();
-
-            app.UseMvc();
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("-- Demo.AspNetCore.Changefeed --");
-            });
+            app.UseWebSockets()
+                .UseStaticFiles()
+                .UseRouting()
+                .UseEndpoints(endpoints =>
+                {
+                    endpoints.MapServerSentEvents("/thread-stats-changefeed");
+                    endpoints.MapWebSocketConnections("/thread-stats-changefeed-fallback");
+                })
+                .Run(async (context) =>
+                {
+                    await context.Response.WriteAsync("-- Demo.AspNetCore.Changefeed --");
+                });
         }
     }
 }
