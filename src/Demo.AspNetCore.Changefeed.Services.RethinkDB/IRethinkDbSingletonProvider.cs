0deeb66d04c05961536165baf6c31b408839d161
Tomasz Pęczek <tpeczek@gmail.com>
2023-09-20 22:08:42 +0000

    Adding Azure Blob Storage change feed implementation

--- src/Demo.AspNetCore.Changefeed.Services.RethinkDB/IRethinkDbSingletonProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Demo.AspNetCore.Changefeed.Services.RethinkDb
-{
-    internal interface IRethinkDbSingletonProvider
-    {
-        global::RethinkDb.Driver.RethinkDB RethinkDbSingleton { get; }
-
-        global::RethinkDb.Driver.Net.Connection RethinkDbConnection { get; }
-    }
-}
